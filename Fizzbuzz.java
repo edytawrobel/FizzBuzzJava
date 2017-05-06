b879de5d4d35a5071b52968fb3f3c35f424d5a13
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-05-06 22:09:40 +0100

    final condition works, given 15 returns fizzbuzz, all tests passing

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -2,9 +2,11 @@ public class Fizzbuzz {
 
   public String getResult(int number) {
 
-    if (number % 3 == 0) {
+    if (number % 15 == 0) {
+      return "FizzBuzz";
+    } else if (number % 3 == 0) {
       return "Fizz";
-    } else if (number % 5 == 0) {
+    } else if (number % 5 == 0 ) {
       return "Buzz";
     } else {
       return String.valueOf(number);
